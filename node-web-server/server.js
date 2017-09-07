f2e1e073755af0eba1093bcee2636b1ac4d83f90
vipzmangukiya <vbmm20@gmail.com>
2017-09-07 07:55:39 +0530

    Complete Creating Web Server Complete Template Rendering,Middleware Server

---
+++ node-web-server/server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const hbs 	  = require('hbs');
+const fs 	  = require('fs');
+
+var app 	= express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine','hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();	
+});
+
+hbs.registerHelper('StrinUpperCase', (string) => {
+	return string.toUpperCase();
+})
+
+
+
+app.use((req, res, next )=>{
+	var now = new Date().toString();
+	var log = `${now}: ${req.method}  ${req.url}`;
+	fs.appendFile('serever.log', log +'\n', (err)=>{
+		if(err) {
+			console.log('Unable to append Server.log.');
+		}
+	});
+	next(); 
+});
+
+app.use((req,res,next)=>{
+	res.render('maintenance.hbs');
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+	//res.send('Hello Express !');
+	// res.send({
+	// 	name: 'Vipul',
+	// 	Likes: [
+	// 		'Biking',
+	// 		'Cities'
+	// 	]
+	// });
+
+	res.render('home.hbs',{
+		pageTitle:'Home Page',
+		message:'Welcome to my First site'
+	});
+});
+
+
+app.get('/about', (req,res) => {
+	res.render('about.hbs',{
+		pageTitle: 'About Us Page'
+	});
+});
+
+app.get('/bad', (req,res) => {
+
+	res.send({
+		errorMessage: 'Unable to handle request'
+	});
+});
+
+
+app.listen(3000,() => {
+	console.log('Server is up on port 3000');
+});
+
--- weather-app/app-promise.js
+++ weather-app/app-promise.js
@@ -32,16 +32,18 @@ axios.get(geocodeUrl).then((response) => {
 	var lat 		= response.data.results[0].geometry.location.lat;
 	var lng 		= response.data.results[0].geometry.location.lng;
 	var weatherUrl  = `https://api.forecast.io/forecast/4a04d1c42fd9d32c97a2c291a32d5e2d/${lat},${lng}`;
+	
 	console.log('Location:',response.data.results[0].formatted_address);
-	return axios.get(weatherUrl);
-
+	return axios.get(weatherUrl); // nested axios call 
 }).then((response) => {
+
 	var temperature = response.data.currently.temperature;
 	var apparentTemperature = response.data.currently.apparentTemperature;
 
 	console.log(`Weather Are : It's currently ${temperature}. It feels like ${apparentTemperature} `);
 
 }).catch((e) => {
+
 	if(e.code === 'ENOTFOUND') {
 		console.log('Unable to Connect API Servers.');
 	} else {
